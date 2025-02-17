539c0ec2a110dbfddc801976f6c7157010c47058
Chris Krycho <hello@chriskrycho.com>
2025-02-16 20:41:05 -0700

    Integrate with Shuttle and Axum
    This actually works! 🎉

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,101 @@
 mod b2;
 mod image;
 
-use std::io;
+use std::process;
+
+use axum::{
+    body::Body,
+    extract::{Query, State},
+    http::{self, HeaderMap, Response},
+    response::IntoResponse,
+    routing, Router,
+};
+use reqwest::header;
+use sha1::{Digest, Sha1};
+use shuttle_runtime::SecretStore;
 
 const GIT_SHA: &str = env!("GIT_SHA");
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Error> {
-    let data = image::render("Tidy First? A Personal Exercise in Empirical Software Design");
-    let file_name = "testing.png";
+#[shuttle_runtime::main]
+async fn main(#[shuttle_runtime::Secrets] secrets: SecretStore) -> shuttle_axum::ShuttleAxum {
+    let auth = Auth::try_from(secrets).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(1);
+    });
 
-    let auth_file = std::fs::read_to_string("Secrets.toml").map_err(|source| Error::Io {
-        message: String::from("Could not read Secrets.toml"),
-        source,
-    })?;
-    let auth: Auth = toml::from_str(&auth_file)?;
+    let app_state = AppState { auth };
 
-    b2::ClientBuilder::new(auth.id, auth.key)
-        .authorize(reqwest::Client::new())
-        .await?
-        .upload_file(file_name, data)
-        .await?;
+    let router = Router::new()
+        .route("/", routing::get(image))
+        .with_state(app_state);
 
-    Ok(())
+    Ok(router.into())
 }
 
-#[derive(Debug, serde::Deserialize)]
-#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[derive(Debug, Clone)]
+struct AppState {
+    auth: Auth,
+}
+
+#[axum::debug_handler]
+async fn image(
+    State(AppState { auth }): State<AppState>,
+    Query(qp): Query<QueryParams>,
+) -> Result<Response<Body>, Error> {
+    let hash = sha1_hash(qp.page_title.as_bytes());
+    let cache_key = format!("{GIT_SHA}+{hash}");
+
+    let mut b2_client = b2::ClientBuilder::new(auth.id, auth.key)
+        .authorize(reqwest::Client::new())
+        .await?;
+
+    let image_data = match b2_client.download_file(&cache_key).await? {
+        Some(data) => data,
+        None => {
+            let data = image::render(&qp.page_title);
+            b2_client.upload_file(&cache_key, &data).await?;
+            data
+        }
+    };
+
+    let mut headers = HeaderMap::new();
+    headers.insert(header::CONTENT_TYPE, "image/png".parse().unwrap());
+    headers.insert(
+        header::CACHE_CONTROL,
+        "public, max-age=31536000".parse().unwrap(),
+    );
+    headers.insert(header::ETAG, cache_key.parse().unwrap());
+    let response = (headers, image_data).into_response();
+    Ok(response)
+}
 
 fn sha1_hash(data: &[u8]) -> String {
     let mut hasher = Sha1::new();
     hasher.update(data);
     format!("{:x}", hasher.finalize())
 }
+#[derive(Clone, Debug)]
 struct Auth {
     id: String,
     key: String,
+}
+
+impl TryFrom<SecretStore> for Auth {
+    type Error = MissingSecrets;
+
+    fn try_from(secrets: SecretStore) -> Result<Self, Self::Error> {
+        match (secrets.get("ID"), secrets.get("KEY")) {
+            (Some(id), Some(key)) => Ok(Auth { id, key }),
+            (Some(_), None) => Err(MissingSecrets::Key),
+            (None, Some(_)) => Err(MissingSecrets::Id),
+            (None, None) => Err(MissingSecrets::Both),
+        }
+    }
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct QueryParams {
+    page_title: String,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -45,13 +105,20 @@ enum Error {
         #[from]
         source: b2::Error,
     },
+}
 
-    #[error("{message}: {source}")]
-    Io { message: String, source: io::Error },
+#[derive(Debug, thiserror::Error)]
+enum MissingSecrets {
+    #[error("Missing `KEY`")]
+    Key,
+    #[error("Missing `ID`")]
+    Id,
+    #[error("Missing `KEY` and `ID`")]
+    Both,
+}
 
-    #[error("Could not deserialize secrets")]
-    DeserializeSecrets {
-        #[from]
-        source: toml::de::Error,
-    },
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        (http::StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
+    }
 }
